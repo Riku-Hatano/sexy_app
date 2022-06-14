99ea6931eb917026d8278f019086b71b19db200f
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-06-14 14:24:13 +0900

    post機能を追加しようとしたら失敗しました

--- controller.go
+++ controller.go
@@ -12,6 +12,7 @@ import (
 )
 
 func show(c echo.Context) error {
+	fmt.Println("done")
 	name := c.QueryParam("name")
 	email := c.QueryParam("email")
 	id, _ := strconv.Atoi(c.QueryParam("id"))
@@ -26,22 +27,26 @@ func show(c echo.Context) error {
 	//このuをJSON形式に直す
 }
 
-type RequestBody struct {
-	Something string
-}
-
 func postData(c echo.Context) error {
-	requestBody := &RequestBody{
-		Something: "Hello",
-	}
+	// requestBody := &RequestBody{
+	// 	Something: "Hello",
+	// }
+	fmt.Println("done")
+	u := new(User)
+	name := c.QueryParam("name")
+	email := c.QueryParam("email")
+	id, _ := strconv.Atoi(c.QueryParam("id"))
+	u.Email = email
+	u.Name = name
+	u.Id = id
 
-	jsonString, err := json.Marshal(requestBody)
+	jsonString, err := json.Marshal(u)
 	if err != nil {
 		panic("Error")
 	}
 
 	endpoint := "http://localhost:1323/users"
-	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonString))
+	req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(jsonString))
 	if err != nil {
 		panic("Error")
 	}
@@ -60,6 +65,6 @@ func postData(c echo.Context) error {
 		panic("Error")
 	}
 
-	fmt.Printf("%#v", string(byteArray))
-	return c.String(http.StatusOK, "OK")
+	fmt.Println(string(byteArray))
+	return c.JSON(200, u)
 }
--- searcher.go
+++ searcher.go
@@ -18,13 +18,21 @@ func main() {
 	// ミドルウェアを設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
 	// ルートを設定
 	e.GET("/user", show)
 	// サーバーをポート番号1323で起動
 	e.Logger.Fatal(e.Start(":1323"))
-
-	//post
 	e.POST("/users", postData)
-
+	//post
+	// ps := url.Values{}
+	// ps.Add("id", "1")
+	// ps.Add("name", "moripi")
+	// fmt.Println(ps.Encode())
+	// res, err := http.PostForm("http://localhost:1323/users", ps)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer res.Body.Close()
+	// body, _ := io.ReadAll(res.Body)
+	// fmt.Print(string(body))
 }
